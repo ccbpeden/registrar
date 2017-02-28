d437834eac51a0bda3672d97daca61b5a1469f2c
Charles Peden and Jay Freeman <charles@>
2017-02-28 10:32:02 -0800

    add code/test for setters

--- tests/StudentTest.php
+++ tests/StudentTest.php
@@ -29,5 +29,20 @@
 
             $this->assertEquals([$student_name, $admission_date], $result);
         }
+
+        function test_setters()
+        {
+            $student_name = "Vasily Raskolnikov";
+            $admission_date = "1897-03-04";
+            $new_student = new Student($student_name, $admission_date);
+            $new_name = "Boris Badinoff";
+            $new_admission_date = "1956-06-14";
+
+            $new_student->setName($new_name);
+            $new_student->setAdmissionDate($new_admission_date);
+            $result = array($new_student->getName(), $new_student->getAdmissionDate());
+
+            $this->assertEquals([$new_name, $new_admission_date], $result);
+        }
     }
 ?>
